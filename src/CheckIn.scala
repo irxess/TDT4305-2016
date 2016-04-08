d9b960cd5998e13f52e2960cf58859f24b5f5676
Christoffer Viken <christoffer@viken.me>
2016-04-08 14:22:39 +0200

    Optimized part1 and tuned part2.

--- src/CheckIn.scala
+++ src/CheckIn.scala
@@ -2,59 +2,58 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.Locale
 
-import org.apache.spark.rdd.RDD
-
 /**
   * Created by CVi on 29.03.2016.
   */
 @SerialVersionUID(100L)
 class CheckIn(line: String, cities: Array[City]) extends java.io.Serializable {
-  private val data = line.split("\t")
   val id = data(0)
   val uid = data(1).toInt
   val sid = data(2)
   val time = getLocalTime(data(3), data(4).toInt)
   val lat = data(5).toDouble
   val lon = data(6).toDouble
-  //def cat = data(7)
-  //def subCat = data(8)
-  private val city = closestCity(cities, lon, lat)
   val city_name = city.name
   //def country = city.country
   val country_code = city.country_code
+  private val data = line.split("\t")
+  //def subCat = data(8)
+  private val city = closestCity(cities, lon, lat)
+
+  def cat = data(7)
 
   override def toString(): String = "(" + id + ", " + sid + ", " + city + ")"
 
-  //Blatantly cooked from https://rosettacode.org/wiki/Haversine_formula#Scala
-  def haversine(lat1:Double, lon1:Double, lat2:Double, lon2:Double)={
-    val R = 6372.8
-    val dLat=(lat2 - lat1).toRadians
-    val dLon=(lon2 - lon1).toRadians
-
-    val a = Math.pow(Math.sin(dLat/2),2) + Math.pow(Math.sin(dLon/2),2) * Math.cos(lat1.toRadians) * Math.cos(lat2.toRadians)
-    val c = 2 * Math.asin(Math.sqrt(a))
-    R * c
+  def getLocalTime(utcString: String, localOffset: Int): LocalDateTime = {
+    val dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH)
+    val utcTime = LocalDateTime.parse(utcString, dateFormat)
+    utcTime.plusMinutes(localOffset)
   }
 
-  def getLocalTime( utcString: String, localOffset: Int): LocalDateTime = {
-    val dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH)
-    val utcTime = LocalDateTime.parse( utcString, dateFormat )
-    utcTime.plusMinutes( localOffset )
+  def closestCity(cities: Array[City], lon: Double, lat: Double): City = {
+    var current = new City("N/A\t0.0\t0.0\tNA\tNA")
+    var distance = Double.PositiveInfinity
+    for (i <- cities.indices) {
+      if (distance_between(cities(i).lat, cities(i).lon, lat, lon) < distance) {
+        distance = distance_between(cities(i).lat, cities(i).lon, lat, lon)
+        current = cities(i)
+      }
+    }
+    current
   }
 
   def distance_between(lat_1: Double, lon_1: Double, lat_2: Double, lon_2: Double): Double ={
     haversine(lat_1, lon_1, lat_2, lon_2)
   }
 
-  def closestCity(cities: Array[City], lon: Double, lat: Double): City ={
-    var current = new City("N/A\t0.0\t0.0\tNA\tNA")
-    var distance = Double.PositiveInfinity
-    for(i <- cities.indices){
-      if(distance_between(cities(i).lat, cities(i).lon, lat, lon) < distance){
-        distance = distance_between(cities(i).lat, cities(i).lon, lat, lon)
-        current = cities(i)
-      }
-    }
-    current
+  //Blatantly cooked from https://rosettacode.org/wiki/Haversine_formula#Scala
+  def haversine(lat1: Double, lon1: Double, lat2: Double, lon2: Double) = {
+    val R = 6372.8
+    val dLat = (lat2 - lat1).toRadians
+    val dLon = (lon2 - lon1).toRadians
+
+    val a = Math.pow(Math.sin(dLat / 2), 2) + Math.pow(Math.sin(dLon / 2), 2) * Math.cos(lat1.toRadians) * Math.cos(lat2.toRadians)
+    val c = 2 * Math.asin(Math.sqrt(a))
+    R * c
   }
 }
--- src/P2.scala
+++ src/P2.scala
@@ -11,40 +11,73 @@ object P2 {
   val daysOfWeek = Array("-1", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
 
   def main(args: Array[String]) {
-    val file = System.getenv("DS_FILE")
+    /** **
+      * Main program,
+      *
+      * Filters tweets from file (arg0) on
+      * COUNTRY_CODE
+      * LANG
+      * PLACE_TYPE
+      *
+      * Originally set up to use ENV-variables to hold the values,
+      * this version uses hard coded strings
+      */
+    //val file = System.getenv("DS_FILE")
+    val file = args(0)
     val minusFile = System.getenv("DS_FILE_MINUS")
+    //val minusFile = "/path/to/neegative/words.txt"
     val plusFile = System.getenv("DS_FILE_PLUS")
-    val outFile = System.getenv("OUT_FILE")
-    val country = System.getenv("FILTER_COUNTRY")
-    val lang = System.getenv("FILTER_LANG")
-    val placeType = System.getenv("FILTER_PLACE_TYPE")
-    val conf = new SparkConf().setAppName("Simple Application").setMaster("local[4]")
+    //val plusFile = "/path/to/positive/words.txt"
+    //val outFile = System.getenv("OUT_FILE")
+    val outFile = args(1)
+    //val country = System.getenv("FILTER_COUNTRY")
+    val country = "US"
+    //val lang = System.getenv("FILTER_LANG")
+    val lang = "en"
+    //val placeType = System.getenv("FILTER_PLACE_TYPE")
+    val placeType = "city"
+    val conf = new SparkConf().setAppName("Simple Application")
     val sc = new SparkContext(conf)
 
+    //First set up a filtered version
     val twitterData = sc.textFile(file, 2).map(x => x.split("\t"))
+    //Doing it all in one map is way faster than chaining.
+    //We could have implemented optional filters, and gotten much more flexible code
+    //but this is built for speed
     val tweetFiltered = twitterData.filter(x => x(2).equals(country) && x(5).equals(lang) && x(3).equals(placeType))
 
+    //Build the positive and negative words list.
     val wordsMinus = sc.textFile(minusFile, 2).map(s => s -> -1)
     val wordsPlus = sc.textFile(plusFile, 2).map(s => s -> 1)
     val words = wordsMinus.union(wordsPlus)
-
     val wordMap = mutable.HashMap[String, Int]()
     words.collect().foreach(x => wordMap += x)
 
+    /*
+    Calculate local time, city and scores
+     */
     tweetFiltered.map(x => {
       val cal = Calendar.getInstance()
       val time = x(0).toLong + (x(8).toLong * 60)
       cal.setTimeInMillis(time * 1000)
       var score = 0
+      //For each word in tweet
       x(10).split(" ").foreach(w => {
         val k = w.toLowerCase
+        //If the scoring-map has the word
+        // add score.
         if (wordMap contains k) {
           score += wordMap(k)
         }
       })
-      ((x(4), daysOfWeek(cal.get(Calendar.DAY_OF_WEEK))), (score, 1))
-    }).reduceByKey((v1: (Int, Int), v2: (Int, Int)) => (v1._1 + v2._1, v1._2 + v1._2))
-      .map(x => x._1._1 + "\t" + x._1._2 + "\t" + x._2._1.toString)
+      ((x(4), daysOfWeek(cal.get(Calendar.DAY_OF_WEEK))), score)
+    })
+      //Sum up the scores.
+      //Depending on data types aggregateByKey may be faster.
+      //Not in this case it seemed.
+      .reduceByKey((v1, v2) => v1 + v2)
+      //.aggregateByKey(0)((v1, v2) => v1 + v2, (v1, v2) => v1 + v2)
+      .map(x => x._1._1 + "\t" + x._1._2 + "\t" + x._2)
       .saveAsTextFile(outFile)
 
     /*val writer = new PrintWriter(new File(out_file+"3.txt" ))
