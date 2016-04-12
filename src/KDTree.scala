48d499e49806ddbea9831797ddde329e8f05dcf0
Ilse Visser <irxess@gmail.com>
2016-04-12 11:21:28 +0200

    Rework the KD-tree
    The class is still missing tree-generating, and distance calculation could be better

--- src/KDTree.scala
+++ src/KDTree.scala
@@ -2,155 +2,21 @@
   * Created by ilsegv on 05/04/16.
   */
 
-class KDTree(cities: Array[City]) extends Serializable {
 
-    class Node(l: Double, o: Double ) extends java.io.Serializable {
-        val lat = l
-        val lon = o
-        var city:City = _
-        var leftChild = None: Option[Node]
-        var rightChild = None: Option[Node]
+abstract class KDTree(c: City, left: Option[KDTree], right: Option[KDTree]) extends Serializable {
+    val leftChild = left
+    val rightChild = right
+    val city = c
 
-        def this(c: City) {
-            this(c.lat, c.lon)
-            this.city = c
-        }
+    def lat = {city.lat}
+    def lon = {city.lon}
 
-        def insert(n: Node, useLatitude: Boolean) {
-            if (useLatitude) {
-                if (n.lat < lat) { //latitude
-                    // insert to the left
-                    if (leftChild.isEmpty) {
-                        leftChild = Some(n)
-                    } else {
-                        leftChild.get.insert( n, !useLatitude )
-                    }
-                } else {
-                    // insert to the right
-                    if (rightChild.isEmpty) {
-                        rightChild = Some(n)
-                    } else {
-                        rightChild.get.insert( n, !useLatitude )
-                    }
-                }
-            } else {
-                if (n.lon < lon) { //longitude
-                    // insert to the left
-                    if (leftChild.isEmpty) {
-                        leftChild = Some(n)
-                    } else {
-                        leftChild.get.insert( n, !useLatitude )
-                    }
-                } else {
-                    // insert to the right
-                    if (rightChild.isEmpty) {
-                        rightChild = Some(n)
-                    } else {
-                        rightChild.get.insert( n, !useLatitude )
-                    }
-                }
-            }
-        }
-        def distance(n: Node): Double = {
-            haversine( lat, lon, n.lat, n.lon)
-        }
-
-        def searchNode(point: Node, useLatitude: Boolean, bestNode: Node, bestDistance: Double): (Node, Double) = {
-            var currentDistance = distance(point)
-            var currentNode = this
-            // Here we should eliminate the current subtree, if it is not interesting
-//            if (currentDistance > bestDistance) {
-//                return (bestNode, bestDistance)
-//            }
-
-            // at this point, currentDistance is better than bestDistance
-
-            // when at a leaf node, return as current best
-            if (leftChild.isEmpty && rightChild.isEmpty) {
-                return (this, distance(point))
-            }
-
-            var newDistance:Double = Double.MaxValue
-            var result:Node = this
-            var t:(Node, Double) = (result,newDistance)
-            // search the most promising subtree
-            if (isSmallerInDim(point, useLatitude)) {
-                if (leftChild.isDefined){
-                    t = leftChild.get.searchNode(point, !useLatitude, this, currentDistance)
-                    result = t._1
-                    newDistance = t._2
-                    if (newDistance < currentDistance) {
-                        currentDistance = newDistance
-                        currentNode = result
-                    }
-                }
-                if (rightChild.isDefined){
-                    t = rightChild.get.searchNode(point, !useLatitude, this, currentDistance)
-                    result = t._1
-                    newDistance = t._2
-                    if (newDistance < currentDistance) {
-                        currentDistance = newDistance
-                        currentNode = result
-                    }
-                }
-            } else {
-                if (rightChild.isDefined) {
-                    t = rightChild.get.searchNode(point, !useLatitude, this, currentDistance)
-                    result = t._1
-                    newDistance = t._2
-                    if (newDistance < currentDistance) {
-                        currentDistance = newDistance
-                        currentNode = result
-                    }
-                }
-                if (leftChild.isDefined){
-                    t = leftChild.get.searchNode(point, !useLatitude, this, currentDistance)
-                    result = t._1
-                    newDistance = t._2
-                    if (newDistance < currentDistance) {
-                        currentDistance = newDistance
-                        currentNode = result
-                    }
-                }
-            }
-
-
-            return (currentNode, currentDistance)
-        }
-
-        def isSmallerInDim(point: Node, useLatitude: Boolean): Boolean = {
-            if (useLatitude) {
-                if (point.lat < lat) {
-                    true
-                } else {
-                    false
-                }
-            } else {
-                if (point.lon < lon) {
-                    true
-                } else {
-                    false
-                }
-            }
-
-        }
+    def distance(n: SearchNode): Double = {
+        haversine( lat, lon, n.lat, n.lon)
     }
 
-    private val medianCity = cities( cities.sortBy(city => city.lon).length/2 )
-    var rootNode = new Node(medianCity)
-    cities.foreach( city => insertNode(city) )
+    abstract def shortestDistance(n: SearchNode): Double
 
-    def insertNode(city: City) = {
-        rootNode.insert( new Node(city), true )
-    }
-
-    def findNearestNode(lat: Double, lon: Double): City = {
-        val point = new Node(lat, lon)
-        val (n, _) = rootNode.searchNode(point, true, rootNode, Double.MaxValue)
-        n.city
-    }
-
-    // Should use haversine in CheckIn instead
     def haversine(lat1:Double, lon1:Double, lat2:Double, lon2:Double)={
         val R = 6372.8
         val dLat=(lat2 - lat1).toRadians
@@ -160,8 +26,61 @@ class KDTree(cities: Array[City]) extends Serializable {
         val c = 2 * Math.asin(Math.sqrt(a))
         R * c
     }
+
+    abstract def pointIsToTheLeft(point: SearchNode): Boolean
+
+    def nearestNeighbor(point: SearchNode): Unit = {
+        // if tree is empty, return Double.max
+
+        val newDistance = distance(point)
+        if (newDistance < point.distance) point.distance = newDistance
+
+        var mostPromisingTree: Option[KDTree] = _
+        var lessPromisingTree: Option[KDTree] = _
+        if (pointIsToTheLeft(point)) {
+            mostPromisingTree = leftChild
+            lessPromisingTree = rightChild
+        } else {
+            mostPromisingTree = rightChild
+            lessPromisingTree = leftChild
+        }
+
+        if (mostPromisingTree.isDefined) {
+            mostPromisingTree.get.nearestNeighbor(point)
+        }
+
+        if (lessPromisingTree.isDefined) {
+            val minDistanceToUnknownArea = shortestDistance(point)
+            if (minDistanceToUnknownArea < point.distance) {
+                lessPromisingTree.get.nearestNeighbor(point)
+            }
+        }
+    }
+
 }
 
 
+class LatTree(c: City, left: Option[KDTree], right: Option[KDTree]) extends KDTree(c, left, right) {
+    override def pointIsToTheLeft(point: SearchNode): Boolean = { point.lat < lat }
+
+    override def shortestDistance(point: SearchNode): Double = {
+        haversine( lat, point.lon, point.lat, point.lon )
+    }
+}
+
+class LonTree(c: City, left: Option[KDTree], right: Option[KDTree]) extends KDTree(c, left, right) {
+    override def pointIsToTheLeft(point: SearchNode): Boolean = { point.lon < lon }
+
+    override def shortestDistance(point: SearchNode): Double = {
+        haversine( point.lat, lon, point.lat, point.lon)
+    }
+}
+
+class SearchNode(a: Double, o: Double) {
+    // stuff with the point to search for
+    val lat = a
+    val lon = o
+    var distance = Double.MaxValue
+}
 
 
